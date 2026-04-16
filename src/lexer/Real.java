7f8008959f8a41338f37be6ff03a5cdf9d35db57
ipaixaolt <isadellispaixao@gmail.com>
2026-04-16 19:51:07 -0300

    Ajustes para reconhecer a linguagem, separação de pastas e criação da main para rodar testes

--- src/lexer/Real.java
+++ src/lexer/Real.java
@@ -1,12 +1,16 @@
 package lexer;
 
-public class Real extends Token{
+public class Real extends Token {
+
     public final float value;
-    public Real(float v){
+
+    public Real(float v) {
         super(Tag.REAL);
         value = v;
     }
-    public String toString(){
+
+    @Override
+    public String toString() {
         return "" + value;
     }
 }
